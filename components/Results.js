f09f9013f3d023a86d9ee984a2f486454b3acafb
haiderAtGitHub <haiderabbas1982000@gmail.com>
2021-08-18 16:47:22 +0500

    Completed result component

--- components/Results.js
+++ components/Results.js
@@ -1,12 +1,14 @@
 import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
+//for flipmove to work, our result must have valid key
 
 function Results({ results }) {
 	return (
-		<div>
+		<FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
 			{results.map((result) => (
 				<Thumbnail key={result.id} result={result}></Thumbnail>
 			))}
-		</div>
+		</FlipMove>
 	);
 }
 
